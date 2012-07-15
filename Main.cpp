0d9228f2bbe37b08072b4b583c4f503debfda53f
Kim Oliver Rinnewitz <krinnewitz@uos.de>
2012-07-15 17:42:25 +0200

    Made AutoCorr class

--- Main.cpp
+++ Main.cpp
@@ -1,8 +1,7 @@
 #include <stdlib.h>
+#include "AutoCorr.hpp"
 #include <stdio.h>
 #include <math.h>
-#include <opencv/cv.h>
-#include <opencv/highgui.h>
 #include <iostream>
 
 /**
@@ -29,7 +28,7 @@ using namespace std;
  * \return	The normed correlation value at the position determined by
  *		x and y
  */
-double autocorr(int y, int x, cv::Mat &img)
+double auto_corr(int y, int x, cv::Mat &img)
 {
 	cv::Mat_<uchar>& ptrImg = (cv::Mat_<uchar>&)img;
 	int N = img.size().width * img.size().height;
@@ -93,275 +92,6 @@ void autocov(const cv::Mat &img, cv::Mat &dst)
 	dst = dst * (1/(denom * 1.0/(img.rows*img.cols)));
 }
 
-/**
- * \brief 	Implementation of the auto correlation function using fourier transformation.
- *		This implementation is quite fast and may be used for productive jobs. Auto
- *		correlation can be calculated by transforming the image img into the frequency
- *		domain (getting the fourier transformation IMG of img), calculating
- *		IMG * IMG  and transforming the result back to the image domain. 
- *
- * \param 	img	The image to calculate the auto correlation for. Must be one channel
- *			gray scale.
- * \param	dst	The destination to store the correlation values in. The result is normed.
- */
-void autocorrDFT(const cv::Mat &img, cv::Mat &dst)
-{
-	//Convert image from unsigned char to float matrix
-	cv::Mat fImg;
-	img.convertTo(fImg, CV_32FC1);
-	//Subtract the mean
-	cv::Mat mean(fImg.size(), fImg.type(), cv::mean(fImg));
-	cv::subtract(fImg, mean, fImg);
-	
-	//Calculate the optimal size for the dft output.
-	//This increases speed.
-	cv::Size dftSize;
-	dftSize.width = cv::getOptimalDFTSize(2 * img.cols +1 );
-	dftSize.height = cv::getOptimalDFTSize(2 * img.rows +1);
-	
-	//prepare the destination for the dft
-	dst = cv::Mat(dftSize, CV_32FC1, cv::Scalar::all(0));
-	
-	//transform the image into the frequency domain
-	cv::dft(fImg, dst);
-	//calculate DST * DST (don't mind the fourth parameter. It is ignored)
-	cv::mulSpectrums(dst, dst, dst, cv::DFT_INVERSE, true);
-	//transform the result back to the image domain 
-	cv::dft(dst, dst, cv::DFT_INVERSE | cv::DFT_SCALE);
-
-	//norm the result
-	cv::multiply(fImg,fImg,fImg);
-	float denom = cv::sum(fImg)[0];
-	dst = dst * (1/denom);
-
-}
-
-/**
- * \brief	Calculates the sum of all rows for each column of
- *		the given autocorrelation matrix. It will return
- *		a float array of length ac.cols.
- *
- * \param	ac	The auto correlation matrix
- * \param	output	The destination where results are stored
- */
-void getACX(const cv::Mat &ac, float* &output)
-{
-	//Allocate output
-	output = new float[ac.cols];
-	for(int x = 0; x < ac.cols; x++)
-	{
-		float rho_x = 0;
-		for(int y = 0; y < ac.rows; y++)
-		{
-			rho_x += ac.at<float>(y,x);
-		}
-		output[x] = rho_x;
-	}
-}
-
-/**
- * \brief	Calculates the sum of all columns for each row of
- *		the given autocorrelation matrix. It will return
- *		a float array of length ac.rows.
- *
- * \param	ac	The auto correlation matrix
- * \param	output	The destination where results are stored
- */
-void getACY(const cv::Mat &ac, float* &output)
-{
-	//Allocate output
-	output = new float[ac.rows];
-	for(int y = 0; y < ac.rows; y++)
-	{
-		float rho_y = 0;
-		for(int x = 0; x < ac.cols; x++)
-		{
-			rho_y += ac.at<float>(y,x);
-		}
-		output[y] = rho_y;
-	}
-}
-
-/**
- * \brief 	Calculates the standard deviation of the given 
- *		data array
- *
- * \param	data	The data array
- * \param	len 	The length of the data array
- *
- * \return 	The standard deviation of the given data array
- */
-float calcStdDev(const int* data, int len)
-{
-	float result = 0;
-	float mean = 0;
-	for (int i = 0; i < len; i++)
-	{
-		mean += data[i];
-	}	
-	mean /= len;
-
-	for (int i = 0; i < len; i++)
-	{
-		result += (data[i] - mean) * (data[i] - mean);
-	}
-	result /= len;
-	result = sqrt(result);
-
-	return result;
-}
-
-/**
- * \brief 	Counts the number of peaks in the given data Array and
- * 		returns the standard deviation of the distances between
- *		the peaks
- * \param	data		The data array
- * \param	stdDev		The standard deviation of the distances
- *				between the peaks
- * \param	len		The length of the array
- *
- * \return	The number of peaks in the data array
- */
-int countPeaks(const float* data, float &stdDev, int len)
-{
-	const float epsilon = 0.0001;
-	int result = 0;
-
-	if (len < 2)
-	{
-		return 0;
-	}
-
-	int lastPeak = -1;
-
-	bool curr_up = true;
-
-	//Count boarders, too
-	if (data[0] > data[1])
-	{
-		result++;
-		lastPeak = 0;
-		curr_up = false;
-	}
-
-	int* distances = new int[len];
-
-	//Search for peaks
-	for (int i = 1; i < len - 1; i++)
-	{
-		bool next_up = curr_up;
-//		if (data[i] > data[i-1])  
-		if (data[i] - data[i-1] > epsilon)
-		{
-			next_up = true;
-		}
-//		if (data[i] < data[i-1])  
-		if (data[i] - data[i-1] < -epsilon)  
-		{
-			next_up = false;
-		}
-		if (next_up == false && curr_up == true)
-		{
-			//peak detected
-			if (lastPeak != -1)
-			{
-				distances[result-1] = lastPeak - i;
-			}
-			result++;
-			lastPeak = i;
-		}
-		curr_up = next_up;
-	}
-
-	if (data[len-1] > data[len-2])
-	{
-		if (lastPeak != -1)
-		{
-			distances[result-1] = lastPeak - (len - 1);
-		}
-		result++;
-	}
-
-	stdDev = calcStdDev(distances, result - 1);
-
-	return result;
-}
-
-
-/**
- * \brief	Tries to find a pattern in an Image using the auto correlation
- *		function. The result can be interpreted as a rectangle at the
- *		origin (0,0) of the input image with the width of sizeX and the
- * 		height of sizeY.
- *
- * \param	input			The image to find a pattern in. Has to be
-					a three channel	color (RGB) image.
- * \param	sizeX			The resulting x size of the found pattern
- * \param	sizeY			The resulting y size of the found pattern
- * \param	minimalPatternSize	The minimum acceptable x and y size of a
- *					pattern 
- *
- * \return	A confidence between 0 and 1 indicating the degree of success in
- *		extracting a pattern from the given image
- */
-double getMinimalPattern(const cv::Mat &input, unsigned int &sizeX, unsigned int &sizeY, const int minimalPatternSize = 10)
-{
-	const float epsilon = 0.00005;
-
-	//make input gray scale 
-	cv::Mat img;	
-	cv::cvtColor(input, img, CV_RGB2GRAY);
-
-	//calculate auto correlation
-	cv::Mat ac;
-	autocorrDFT(img, ac);
-	cv::Mat_<float>& ptrAc = (cv::Mat_<float>&)ac;
-
-//===========================
-	float *rho_x = 0;
-	float *rho_y = 0;	
-	getACX(ac, rho_x);
-	getACY(ac, rho_y);
-	float stdDevX = 0;
-	float stdDevY = 0;
-	int peaksX = countPeaks(rho_x, stdDevX, ac.cols);
-	int peaksY = countPeaks(rho_y, stdDevY, ac.rows);
-	cout<<"Peaks x:"<<peaksX<<"\t\t StdDev rho_x: "<<stdDevX/(ac.cols / peaksX)<<endl;
-	cout<<"Peaks y:"<<peaksY<<"\t\t StdDev rho_y: "<<stdDevY/(ac.rows / peaksY)<<endl;
-//==========================
-
-	//search minimal pattern i.e. search the highest correlation in x and y direction
-	sizeX = 0;
-	sizeY = 0;
-
-	//y direction
-/*	for (int y = minimalPatternSize; y < ac.size().height / 2; y++)
-	{
-		for(int x = 1; x < ac.cols / 2; x++)
-		{
-			if (ptrAc(y, x) > ptrAc(sizeY, sizeX) + epsilon || sizeY == 0)
-			{
-				sizeY = y;	
-				sizeX = x;
-			}
-		}
-	}
-
-	sizeX = 0;
-	
-	//x direction
-	for (int x = minimalPatternSize; x < ac.size().width / 2; x++)
-	{
-		if (ptrAc(sizeY, x) > ptrAc(sizeY, sizeX) + epsilon || sizeX == 0)
-		{
-			sizeX = x;	
-		}
-	}
-*/	
-	sizeX = 1; sizeY = 1; //TODO: remove
-	return ptrAc(sizeY, sizeX);
-} 
-
 int main (int argc, char** argv)
 {
 
@@ -375,8 +105,9 @@ int main (int argc, char** argv)
 		cv::Mat src = cv::imread(argv[1]);
 
 		//try to find a pattern in the image and get the pattern size
+		lssr::AutoCorr* ac = new lssr::AutoCorr(src);
 		unsigned int sizeX, sizeY;
-		cout<<"confidence: "<<getMinimalPattern(src, sizeX, sizeY, atoi(argv[2]))<<endl;
+		cout<<"confidence: "<<ac->getMinimalPattern(sizeX, sizeY, atoi(argv[2]))<<endl;
 
 		//save the pattern
 		cv::Mat pattern = cv::Mat(src, cv::Rect(0, 0, sizeX, sizeY));
